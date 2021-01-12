62cde0064b256406e94a98e3ac49fa832f3fb6fc
dingjing <13962104109@163.com>
2021-01-12 16:01:39 +0800

    增加配置商户得接口

--- src/main/java/com/jinshipark/hfzf/controller/AdapayController.java
+++ src/main/java/com/jinshipark/hfzf/controller/AdapayController.java
@@ -1,15 +1,21 @@
 package com.jinshipark.hfzf.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import com.huifu.adapay.Adapay;
 import com.huifu.adapay.core.AdapayCore;
 import com.huifu.adapay.core.util.AdapaySign;
+import com.huifu.adapay.model.MerConfig;
 import com.jinshipark.hfzf.config.ADAPayPropertyConfig;
+import com.jinshipark.hfzf.mapper2.JinshiparkApakeyMapper;
+import com.jinshipark.hfzf.model.JinshiparkApakey;
+import com.jinshipark.hfzf.model.JinshiparkApakeyExample;
 import com.jinshipark.hfzf.service.AdapayAliPayService;
 import com.jinshipark.hfzf.service.AdapayWxPubService;
 import com.jinshipark.hfzf.service.LincensePlateService;
 import com.jinshipark.hfzf.service.PrePayService;
 import com.jinshipark.hfzf.utils.JinshiparkJSONResult;
 import com.jinshipark.hfzf.vo.AdapayRequstVO;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +24,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.text.ParseException;
+import java.util.List;
 
 /**
  * 汇付天下相关控制类
@@ -36,6 +43,8 @@ public class AdapayController {
 
     @Autowired
     private PrePayService prePayService;
+    @Autowired
+    private JinshiparkApakeyMapper jinshiparkApakeyMapper;
 
     /**
      * 支付宝APP支付
@@ -147,5 +156,51 @@ public class AdapayController {
         } catch (Exception e) {
             logger.info("异步回调开始，参数，request={}");
         }
+    }
+
+    /**
+     * 添加商户配置
+     */
+    @CrossOrigin
+    @RequestMapping(value = "/addMerConfig", method = RequestMethod.GET)
+    @ResponseBody
+    public JinshiparkJSONResult addMerConfig(@RequestParam(value = "parkId") String parkId) {
+        //判断parkId是否为空
+        if (StringUtils.isBlank(parkId)) {
+            return JinshiparkJSONResult.errorMsg("parkId不能为空");
+        }
+        JinshiparkApakeyExample example = new JinshiparkApakeyExample();
+        JinshiparkApakeyExample.Criteria criteria = example.createCriteria();
+        criteria.andParkidEqualTo(parkId);
+        List<JinshiparkApakey> jinshiparkApakeys = jinshiparkApakeyMapper.selectByExample(example);
+        //判断是有存在要配置得记录
+        if (jinshiparkApakeys.size() == 0) {
+            return JinshiparkJSONResult.errorMsg("不存在该商户配置");
+        }
+        //判断需要配置得记录得参数是否正确
+        JinshiparkApakey jinshiparkApakey = jinshiparkApakeys.get(0);
+        if (jinshiparkApakey.getParkid() == null || jinshiparkApakey.getParkid().equals("")
+                || jinshiparkApakey.getAppid() == null || jinshiparkApakey.getAppid().equals("")
+                || jinshiparkApakey.getRsaprivatekey() == null || jinshiparkApakey.getRsaprivatekey().equals("")
+                || jinshiparkApakey.getApikeylive() == null || jinshiparkApakey.getApikeylive().equals("")
+                || jinshiparkApakey.getApikeytest() == null || jinshiparkApakey.getApikeytest().equals("")) {
+
+            return JinshiparkJSONResult.errorMsg("商户参数配置不正确");
+        }
+        //判断商户配置是否已经初始化过
+        MerConfig config = Adapay.getConfig(jinshiparkApakey.getParkid());
+        if (config != null) {
+            return JinshiparkJSONResult.errorMsg("商户已配置，请勿重复配置！");
+        }
+        MerConfig merConfig = new MerConfig();
+        merConfig.setApiKey(jinshiparkApakey.getApikeylive());
+        merConfig.setApiMockKey(jinshiparkApakey.getApikeytest());
+        merConfig.setRSAPrivateKey(jinshiparkApakey.getRsaprivatekey());
+        try {
+            Adapay.addMerConfig(merConfig, jinshiparkApakey.getParkid());
+        } catch (Exception e) {
+            return JinshiparkJSONResult.errorMsg("增加商户配置异常");
+        }
+        return JinshiparkJSONResult.ok();
     }
 }
